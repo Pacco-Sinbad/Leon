dba53a7442a721b88b9bdc52aaf121d295f7e5ee
Mathias Fischer <mfischerf92@gmail.com>
2023-04-04 14:01:18 -0500

    added quite a bit to the dnd app to start

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -1,17 +1,31 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
+//http://www.dnd5eapi.co/docs/
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
-
+  const choice = document.querySelector('input').value.split(' ').join('-')
+//   console.log(choice)
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}/`
+    document.querySelector('h2').innerText = 'Name?'
+    document.querySelector('h3').innerText = 'Class:'
+    document.querySelector('h4').innerText = 'Sub Classes'
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+       console.log(data)
+       document.querySelector('h2').innerText = `${data.name}`
+       document.querySelector('h3').innerText += `${data.classes[0].name}`
+       for (let i =0 ; i < data.subclasses.length; i++){
+        const li = document.createElement('li')
+        li.textContent =  `${data.subclasses[i].name}`
+        document.querySelector('ul').appendChild(li)
+       }
        
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+     
 }
 
