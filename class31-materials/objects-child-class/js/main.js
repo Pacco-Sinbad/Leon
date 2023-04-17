c394534bf780e12987502e89c954e413265b7814
Mathias Fischer <mfischerf92@gmail.com>
2023-04-16 21:13:33 -0500

    completed class 31 and added class 32 materials

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,32 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+
+class Wildanimal extends Animal{
+    constructor(name,origin,endangered){
+        super(name)
+        this.origin = origin
+        this.endangered = endangered
+    }
+    speak(){
+        console.log(`${this.name} makes a ferocious sound`)
+    }
+}
+
+class Monkey extends Wildanimal {
+    constructor(name,origin,endangered){
+        super(name)
+        super(origin)
+        super(endangered)
+    }
+    swing(){
+        console.log(`${this.name} made like a tree and swung outta here!!!`)
+    }
+}
