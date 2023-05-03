d30742f0f934b6423253c5f015f1ac111500ac0d
Mathias Fischer <mfischerf92@gmail.com>
2023-05-02 21:07:52 -0500

    made it through three more subchapters of objects the basics. so far we havent covered too much that i havent seen before but i did uncover a paragraph that mentions using arrow function because they refer to the outer object. I dont think i would have remembered that but im glad i saw it and understand the importance of arrow function with that regard.

--- object-chapter/main.js
+++ object-chapter/main.js
@@ -1,72 +1,100 @@
-   ///////                    THE JAVASCRIPT LANGUAGE CHAPTER FROM JAVASCRIPT.INFO                     //////
+//    ///////                    THE JAVASCRIPT LANGUAGE CHAPTER FROM JAVASCRIPT.INFO                     //////
    
    
    
-                            ////OBJECTS THE BASICS/////
+//                             ////OBJECTS THE BASICS/////
 
-////OBJECTS////
-let user = {}
-user.name = 'John'
-user.surname = 'Smith'
-user.name = 'Pete'
-delete user.name
-//
-function isEmpty(obj){
-    alert( obj === {} ? true : false)
-}
-////proper solution uses a for key in obj loop. if the loop start that means the object isnt empty.
-function isItEmpty(obj){
-    for (let key in obj){
-        return false
-    } 
-    return true
-}
-//
-function sumOfEmployeeSalaries(obj){
-    let sum = 0
-    for (let key in obj){
-        sum += +(obj[key])
-    }
-    console.log(sum)
-}
+// ////OBJECTS////
+// let user = {}
+// user.name = 'John'
+// user.surname = 'Smith'
+// user.name = 'Pete'
+// delete user.name
+// //
+// function isEmpty(obj){
+//     alert( obj === {} ? true : false)
+// }
+// ////proper solution uses a for key in obj loop. if the loop start that means the object isnt empty.
+// function isItEmpty(obj){
+//     for (let key in obj){
+//         return false
+//     } 
+//     return true
+// }
+// //
+// function sumOfEmployeeSalaries(obj){
+//     let sum = 0
+//     for (let key in obj){
+//         sum += +(obj[key])
+//     }
+//     console.log(sum)
+// }
 
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-  }
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
 
-sumOfEmployeeSalaries(salaries)
+// sumOfEmployeeSalaries(salaries)
 
-//
+// //
 
-function multiplyNumeric(obj){
-    for (let key in obj){
-        if(obj[key] % 1 === 0){
-            obj[key]*= 2
-        }
-    }
-    console.log(obj)
-}
+// function multiplyNumeric(obj){
+//     for (let key in obj){
+//         if(obj[key] % 1 === 0){
+//             obj[key]*= 2
+//         }
+//     }
+//     console.log(obj)
+// }
 
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+//   };
   
-  multiplyNumeric(menu);
+//   multiplyNumeric(menu);
 
   // the proper conditional here would have been == 'number' which makes sense because %1 returns whether the keys value is an integer. floaated values would not work in this instance.
 
   ////OBJECT REFERENCES AND COPYING////
-
-
+  //no tasks
   ////GARBAGE COLLECTION/////
-
-
+  //no tasks
   /////OBJECT MEHTODS, "THIS"////
 
+  let calculator = {
+    read(){
+        this.a = +prompt('select the first value', 0)
+        this.b = +prompt('select the second value', 0)
+    },
+    sum(){
+        return this.a + this.b
+    },
+    mul(){
+        return this. a * this.b
+    }
+  }
+
+  let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this
+    },
+    down() {
+      this.step--;
+      return this
+    },
+    showStep: function() { // shows the current step
+      alert( this.step );
+      return this
+    }
+  };
+
+  ladder.up().up().down().showStep().down().showStep();
 
   ////CONSTRUCTOR, OPERATOR "NEW"////
 
